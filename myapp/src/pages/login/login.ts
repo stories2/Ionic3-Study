c808d0595547d31bf322c8789b5eb07331487f8d
Kim HyunWoo <stories2@naver.com>
2019-01-24 16:58:08 +0900

    Add: LoginPage
    로그인 페이지 추가 및 최 상단 페이지를 로그인 페이지로 변경

---
+++ myapp/src/pages/login/login.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the LoginPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {
+
+  username:string;
+  password:string;
+
+  usernamePlaceHolder:string = "Input Username";
+  passwordPlaceHoldeR:string = "Input Password";
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad LoginPage');
+  }
+
+  login() {
+    console.log('username:', this.username, 'password:', this.password)
+  }
+}
