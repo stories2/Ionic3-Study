750d2cbdea744032cb8a64bc6a1cdb4cb6ef1c69
Kim HyunWoo <stories2@naver.com>
2019-01-29 11:06:29 +0900

    Example of several types of ion-select

--- select-component/src/pages/home/home.ts
+++ select-component/src/pages/home/home.ts
@@ -7,8 +7,26 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  gaming: string = "n64";
+  gender: string = "f";
+  os: string;
+  music: string;
+  month: string;
+  year: number;
 
+  musicAlertOpts: {
+    title: string,
+    subTitle: string
   }
 
+  constructor(public navCtrl: NavController) {
+    this.musicAlertOpts = {
+      title: '1994 Music',
+      subTitle: 'Select your favorite'
+    }
+  }
+
+  stpSelect() {
+    console.log('STP selected')
+  }
 }
