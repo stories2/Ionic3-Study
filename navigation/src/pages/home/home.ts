3ebd1371ccbf3b789d8dc7ca9bc5db028192102f
Kim HyunWoo <stories2@naver.com>
2019-01-29 13:23:18 +0900

    Example of move page and back

--- navigation/src/pages/home/home.ts
+++ navigation/src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { NextPage } from '../next/next'
 
 @Component({
   selector: 'page-home',
@@ -11,4 +12,7 @@ export class HomePage {
 
   }
 
+  moveToNextPage() {
+    this.navCtrl.push(NextPage);
+  }
 }
