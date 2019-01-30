b636752402e7c7acb7c18cc9a86bb63c83cc33f0
Kim HyunWoo <stories2@naver.com>
2019-01-30 16:00:22 +0900

    Example of pass data to next page

--- param/src/pages/home/home.ts
+++ param/src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { NextPage } from '../next/next';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,13 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  inputNumber:number;
+
   constructor(public navCtrl: NavController) {
 
   }
 
+  passDataToNextPage() {
+    this.navCtrl.push(NextPage, {number: this.inputNumber})
+  }
 }
